7609d0c6db51cca54832c81358c350dc5bc33ae6
Joe Webster <joseph.scott.webster@gmail.com>
2020-10-14 17:09:08 -0400

    Click fire button to reveal image

--- main.js
+++ main.js
@@ -1,8 +1,15 @@
+let fireBtn = document.querySelector('button');
 let box = document.querySelector('div');
+let boxImg = document.querySelector('img');
 
 function changeColor() {
   box.classList.toggle('green');
 }
 
+function showImage() {
+  boxImg.classList.toggle('hidden');
+}
+
 box.addEventListener('click', changeColor);
+fireBtn.addEventListener('click', showImage);
 
