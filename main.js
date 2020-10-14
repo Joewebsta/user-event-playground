fbf3f8d07b20ee9d374d459ece0577f594b85867
Joe Webster <joseph.scott.webster@gmail.com>
2020-10-14 17:59:26 -0400

    Type in text input to grow and shrink box

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let body = document.body;
 let fireBtn = document.querySelector('.fireBtn');
 let monkeyBtn = document.querySelector('.monkeyBtn');
+let textInput = document.querySelector('.textInput');
 let box = document.querySelector('div');
 let boxImg = document.querySelector('img');
 
@@ -16,7 +17,26 @@ function showImage() {
   boxImg.classList.toggle('hidden');
 }
 
+function grow() {
+  let width = box.offsetWidth;
+  let height = box.offsetHeight;
+  width += 20;
+  height += 20;
+  box.style.width = `${width}px`;
+  box.style.height = `${height}px`;
+}
+
+function shrink() {
+  let width = box.offsetWidth;
+  let height = box.offsetHeight;
+  width -= 40;
+  height -= 40;
+  box.style.width = `${width}px`;
+  box.style.height = `${height}px`;
+}
+
 box.addEventListener('click', changeColor);
 fireBtn.addEventListener('click', showImage);
 monkeyBtn.addEventListener('click', changeBgColor);
-
+textInput.addEventListener('keyup', grow);
+textInput.addEventListener('keydown', shrink);
