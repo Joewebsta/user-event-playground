736f9b9484c6c4355168ea39c2ab5a863318e7f7
Joe Webster <joseph.scott.webster@gmail.com>
2020-10-14 17:28:17 -0400

    Click monkey button to change background color

--- main.js
+++ main.js
@@ -1,9 +1,15 @@
-let fireBtn = document.querySelector('button');
+let body = document.body;
+let fireBtn = document.querySelector('.fireBtn');
+let monkeyBtn = document.querySelector('.monkeyBtn');
 let box = document.querySelector('div');
 let boxImg = document.querySelector('img');
 
 function changeColor() {
   box.classList.toggle('green');
+}
+
+function changeBgColor() {
+  body.classList.toggle('gray');
 }
 
 function showImage() {
@@ -12,4 +18,5 @@ function showImage() {
 
 box.addEventListener('click', changeColor);
 fireBtn.addEventListener('click', showImage);
+monkeyBtn.addEventListener('click', changeBgColor);
 
